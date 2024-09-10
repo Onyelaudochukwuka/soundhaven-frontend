5a784ea3b9f6959e6951410a147c9024d52efb0d
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-09-10 23:17:03 +0100

    improvements: added portal handler for drag and drop, moved file catcher to windows instance

--- src/components/layout/DragAndDropWrapper.tsx
+++ src/components/layout/DragAndDropWrapper.tsx
@@ -1,28 +1,38 @@
-import React, { FC, useRef, useState } from "react"
-import { FaUpload } from "react-icons/fa";
-import { extractTitleFromFileName } from "../FileUpload";
-import { useTracks } from "@/hooks/UseTracks";
+import React, {FC, useLayoutEffect, useRef, useState} from "react"
+import {FaUpload} from "react-icons/fa";
+import {extractTitleFromFileName} from "../FileUpload";
+import {useTracks} from "@/hooks/UseTracks";
+import {createPortal} from "react-dom";
+
 interface IProps {
 
 }
+
 const DragAndDropWrapper: FC<IProps> = () => {
     const wrapperRef = useRef<HTMLDivElement>(null);
     const fileInputRef = useRef<HTMLInputElement>(null);
     const [showModal, setShowModal] = useState(false);
     const [uploading, setUploading] = useState(false)
-    const { uploadTrack, fetchTracks } = useTracks();
-    const onDragEnter = () => setShowModal(true)
+    const {uploadTrack, fetchTracks} = useTracks();
+    const [portal, setPortal] = useState<Element | null>(null);
+    const onDragEnter = (e: DragEvent) => {
+        e.preventDefault()
+        setShowModal(true)
+    }
     const onDragLeave = () => setShowModal(false)
-    const onDrop = async (event: React.DragEvent<HTMLDivElement>) => {
+    const onDrop = async (event: DragEvent) => {
         event.preventDefault();
-        setShowModal(false)
-        const files = event.dataTransfer.files;
-        if (files.length > 0) {
-            if (fileInputRef.current) {
+        setShowModal(true)
+        const files = event.dataTransfer?.files || null;
+        const itemLength = event.dataTransfer?.items.length || 0;
+        setShowModal(true)
+        if (itemLength > 0) {
+            if (fileInputRef.current?.files) {
                 fileInputRef.current.files = files
                 setUploading(true);
-                for (let i = 0; i < files.length; i++) {
-                    const file: File = files[i]
+                for (let i = 0; i < itemLength; i++) {
+                    const file: File | undefined = files?.[i]
+                    if (!file) continue;
                     const formData = new FormData();
                     formData.append('file', file);
                     formData.append('name', extractTitleFromFileName(file.name));
@@ -43,18 +53,27 @@ const DragAndDropWrapper: FC<IProps> = () => {
                 setUploading(false);
             }
         }
-    };
+        setShowModal(false)
 
-    const handlePreventDefault = (e: React.DragEvent<HTMLDivElement>) => e.preventDefault()
-    return (
-        <React.Fragment key="drag-and-drop">
+    };
+    useLayoutEffect(() => {
+        setPortal(document.body)
+        window.addEventListener('drop', onDrop)
+        window.addEventListener('dragover', onDragEnter)
+        window.addEventListener('dragenter', onDragEnter)
+        window.addEventListener('dragleave', onDragLeave)
+        return () => {
+            window.removeEventListener('drop', onDrop)
+            window.removeEventListener('dragover', onDragEnter)
+            window.removeEventListener('dragenter', onDragEnter)
+            window.removeEventListener('dragleave', onDragLeave)
+        }
+    }, [])
+    return portal && createPortal(
+        <React.Fragment>
             <div
-                onDragEnter={onDragEnter}
-                onDragLeave={onDragLeave}
-                onDrop={onDrop}
-                onDragOver={handlePreventDefault}
                 ref={wrapperRef}
-                className={`grid place-content-center fixed inset-0 w-full h-full ${showModal || uploading ? "bg-black/20" : ''}`}
+                className={`grid place-content-center fixed inset-0 w-full h-full ${showModal || uploading ? "bg-black/20 pointer-events-auto" : 'pointer-events-none'}`}
             >
                 <input
                     ref={fileInputRef}
@@ -65,15 +84,12 @@ const DragAndDropWrapper: FC<IProps> = () => {
             </div>
             {(showModal || uploading) && (
                 <div
-                    onDragEnter={onDragEnter}
-                    onDragLeave={onDragLeave}
                     className="border-2 border-dotted p-8 flex m-auto -z-10 flex-col space-y-4 items-center fixed inset-0 w-fit h-fit">
-                    <FaUpload />
+                    <FaUpload/>
                     <span>{uploading ? "File uploading" : "Drop file To upload"}</span>
                 </div>
             )}
         </React.Fragment>
-
-    )
+        , portal)
 }
 export default DragAndDropWrapper
